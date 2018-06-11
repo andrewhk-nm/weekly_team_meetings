405bf3043ad773dde1ca412f4a22c316b73d4f46
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-11 09:38:45 -0400

    map out some XML data structure from 'CB Monthly Activities and Raios.xml'

--- main.py
+++ main.py
@@ -19,6 +19,13 @@
 import xml.etree.ElementTree as etree
 import sys # for argv
 
+def get_tree(path_and_filename):
+    """ Given the path and filename of an xml doc, return the tree.
+    """
+    tree = etree.parse(path_and_filename)
+    
+    return tree
+
 if __name__ == '__main__':
     # Probably grab the file name from the arg.
     args = sys.argv
@@ -34,6 +41,39 @@ if __name__ == '__main__':
         path_and_filename = input('''Please enter the path and filename for \
 the "CB Monthly Activity and Ratios.xml" XML file. ''')
 
+    # DEBUG print
     print(path_and_filename)
-    
-    input()
+
+    # Parse the xml
+    # Get the etree object
+    tree = get_tree(path_and_filename)
+
+    # CB Monthly Activites and Ratios XML Structure
+    # Are these text box name### fields consistent between reports?
+    # <BusinessActivity2
+    #   Textbox172=[Submitted Lives Data]
+    #   Textbox173=[Submitted Annual Premium Data]
+    #   Textbox174=[Placed Lives Goal Data]
+    #   Textbox175=[Placed NC Goal Data]
+    #   Textbox176=[Placed Annual Premium Goal Data]
+    #   Textbox57=["Total" Label]
+    #   Textbox128=[Submitted Lives Total Data]
+    #   Textbox130=[Submitted Annual Premium Total Data]
+    #   Textbox143=[Placed Lives Total Data]
+    #   Textbox136=[Placed NC Total Data]
+    #   Textbox141=[Placed Annual Premium Total Data]
+    #   Textbox38=["Avg/Mth" Label]
+    #   Textbox30=[Submitted "Avg/Mth" Lives Data]
+    #   Textbox31=[Submitted "Avg/Mth" Annual Prem Data]
+    #   Textbox32=[Placed "Avg/Mth" Lives Data]
+    #   Textbox34=[Placed "Avg/Mth" NC Data]
+    #   Textbox36=[Placed "Avg/Mth" Annual Prem Data]
+    #   Textbox47=[Submitted Lives Data, Annualized]
+    #   Textbox48=[Submitted Annual Prem Data, Annualized
+    #   Textbox50=[Placed Lives Data, Annualized]
+    #   Textbox51=[Placed NC Data, Annualized]
+    #   Textbox53=[Placed Annual Prem Data, Annualized]
+    #   >
+
+    # Don't automatically exit when finished.
+    input('Press <ENTER> to exit')
