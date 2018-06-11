d275b79dd8ccef69df021d72f8a8f31676cdb1b5
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-11 09:20:22 -0400

    get path and filename from arg xor input()

--- main.py
+++ main.py
@@ -14,3 +14,26 @@
 #           Lives: 4 / week
 #           NC: 1.38 / week
 #           Prem: $5770 / week
+#       Compare to last week.
+
+import xml.etree.ElementTree as etree
+import sys # for argv
+
+if __name__ == '__main__':
+    # Probably grab the file name from the arg.
+    args = sys.argv
+
+    ## Make sure it gets the right info from the args.
+    #for arg in args:
+    #    print(arg)
+
+    # If no args are passed, ask for the file path and name.
+    try:
+        path_and_filename = args[1]
+    except(IndexError):
+        path_and_filename = input('''Please enter the path and filename for \
+the "CB Monthly Activity and Ratios.xml" XML file. ''')
+
+    print(path_and_filename)
+    
+    input()
