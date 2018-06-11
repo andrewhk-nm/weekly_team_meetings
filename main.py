f07fe5b1509700de0a3c83adc7edc4397df1e305
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-11 09:03:38 -0400

    create main.py, main_test.py, create abstract structure

---
+++ main.py
@@ -0,0 +1,16 @@
+""" Main module for my weekly report functions.
+"""
+
+## CB Monthly Activities and Ratios
+#   Default filename:
+#       'CB Monthly Activity and Ratios.xml'
+#   Data I want:
+#       Lives
+#       New Clients
+#       Annual Premium
+#   What do I do with it?
+#       Lives / week needed (can use nm.per_week)
+#       Compare to Benchmark
+#           Lives: 4 / week
+#           NC: 1.38 / week
+#           Prem: $5770 / week
