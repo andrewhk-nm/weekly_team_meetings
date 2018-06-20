eee8f2b0bc43f9e1ceadac33eeb284ee2df36eaa
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-20 11:44:30 -0400

    interface outputs the answers (not pretty printed)

--- cb_maarr_interface.py
+++ cb_maarr_interface.py
@@ -1,3 +1,21 @@
 """ The interface portion of the CB Monthly Activities and Ratios Report module.
 """
 
+import sys
+import cb_maarr
+
+if __name__ == "__main__":
+    # Get filename from args, if available
+    try:
+        xml_file = sys.argv[1]
+    except(IndexError):
+        # If no argument was passed, default to None
+        xml_file = None
+    
+    NmCbMaarr = cb_maarr.NmCbMonthlyActivitesAndRatiosReportParser(xml_file)
+    
+    # DEBUG print the results
+    print('NmCbMaarr.lives_per_week={}'.format(NmCbMaarr.lives_per_week))
+    print('NmCbMaarr.new_clients_per_week={}'.format(NmCbMaarr.new_clients_per_week))
+    print('NmCbMaarr.premium_per_week={}'.format(NmCbMaarr.premium_per_week))
+    
