424c21a8583a2bbe87882acb33521ccb25d1d1b7
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-20 11:49:51 -0400

    stub out pretty printed strings

--- cb_maarr_interface.py
+++ cb_maarr_interface.py
@@ -19,3 +19,15 @@ if __name__ == "__main__":
     print('NmCbMaarr.new_clients_per_week={}'.format(NmCbMaarr.new_clients_per_week))
     print('NmCbMaarr.premium_per_week={}'.format(NmCbMaarr.premium_per_week))
     
+
+"""Lives / week needed: {lives_per_week}    {d_lives_last_week} Lives last week
+{d_lives_from_last_time} from last time
+Benchmark: {lives_benchmark}"""
+
+"""NC / week needed: {nc_per_week}    {d_nc_last_week} NC last week
+{d_nc_from_last_time} from last time
+Benchmark: {nc_benchmark}"""
+
+"""Premium / week needed: {premium_per_week}    {d_premium_last_week} Premium last week
+{d_premium_from_last_time} from last time
+Benchmark: {premium_benchmark}"""
