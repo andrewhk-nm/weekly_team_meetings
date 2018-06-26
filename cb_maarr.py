ca3bcb3ca87c3ae041ac473dcc60512a0859c98b
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-06-26 12:38:50 -0400

    all needed historical info should be returned to interface from class

--- cb_maarr.py
+++ cb_maarr.py
@@ -62,10 +62,15 @@ class NmCbMonthlyActivitesAndRatiosReportParser():
     #   >
     
     
-    # Publicly accessable class variables that hold the results
+    # Publicly accessable class variables that hold the results (current)
     lives_per_week = None
     new_clients_per_week = None
     premium_per_week = None
+    
+    # Publicly accessable class variables that hold the results (historical)
+    lives_per_week_hist = None
+    new_clients_per_week_hist = None
+    premium_per_week_hist = None
     
     # Benchmark values
     # Benchmark = Goal / 52 (weeks per year)
@@ -79,12 +84,12 @@ class NmCbMonthlyActivitesAndRatiosReportParser():
               
         # validate the current and historical report xml file
         path_and_filename = self._validate_xml_file(xml_file)
-        path_and_filename_hist = self._validate_xml_file(xml_file_hist)
+        if xml_file_hist is not None: path_and_filename_hist = self._validate_xml_file(xml_file_hist)
                
         # Parse the xml
         # Get the etree object
         root = self._get_root(path_and_filename)
-        root_hist = self._get_root(path_and_filename_hist)
+        if xml_file_hist is not None: root_hist = self._get_root(path_and_filename_hist)
         # DEBUG Print root tree for hist xml file
         print('root_hist={}'.format(root_hist))
         
@@ -95,7 +100,7 @@ class NmCbMonthlyActivitesAndRatiosReportParser():
         # Monthly Activity & Ratio Report (maarr) info that are needed for my weekly reports
         # is called "BusinessActivity2". I don't currently use any of the other info.
         maarr = root.find(xmlns + 'BusinessActivity2')
-        maarr_hist = root_hist.find(xmlns + 'BusinessActivity2')
+        if xml_file_hist is not None: maarr_hist = root_hist.find(xmlns + 'BusinessActivity2')
             
         # Get the current lives goal and data
         lives_goal = float(maarr.get(self._get_fieldname_from_dict('Placed Lives Goal Data')))
@@ -106,51 +111,44 @@ class NmCbMonthlyActivitesAndRatiosReportParser():
         prem_placed = float(maarr.get(self._get_fieldname_from_dict('Placed Annual Premium Total Data')))
 
         # Get the historical lives goal and data
-        lives_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Lives Goal Data')))
-        lives_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Lives Total Data')))
-        nc_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed NC Goal Data')))
-        nc_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed NC Total Data')))
-        prem_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Annual Premium Goal Data')))
-        prem_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Annual Premium Total Data')))
-
+        if xml_file_hist is not None: 
+            lives_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Lives Goal Data')))
+            lives_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Lives Total Data')))
+            nc_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed NC Goal Data')))
+            nc_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed NC Total Data')))
+            prem_goal_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Annual Premium Goal Data')))
+            prem_placed_hist = float(maarr_hist.get(self._get_fieldname_from_dict('Placed Annual Premium Total Data')))
         
-        # # DEBUG print the data I extracted (current)
-        # print('extracted data (current)')
-        # print(lives_goal)
-        # print(lives_placed)
-        # print(nc_goal)
-        # print(nc_placed)
-        # print(prem_goal)
-        # print(prem_placed)
-        
-        # # DEBUG print the data I extracted (historical)
-        # print('extracted data (hist)')
-        # print(lives_goal_hist)
-        # print(lives_placed_hist)
-        # print(nc_goal_hist)
-        # print(nc_placed_hist)
-        # print(prem_goal_hist)
-        # print(prem_placed_hist)
-        
-        # calculate the lives, nc, and prem needed per week.
+        # calculate the lives, nc, and prem needed per week. (current)
         lives_pw = per_week(lives_goal, lives_placed)
         nc_pw = per_week(nc_goal, nc_placed)
         prem_pw = per_week(prem_goal, prem_placed)
         
-        # # DEBUG print the results
-        # print('lives_pw={}'.format(lives_pw))
-        # print('nc_pw={}'.format(nc_pw))
-        # print('prem_pw={}'.format(prem_pw))
+        # calculate the lives, nc, and prem needed per week. (historical)
+        if xml_file_hist is not None: 
+            lives_pw_hist = per_week(lives_goal_hist, lives_placed_hist)
+            nc_pw_hist = per_week(nc_goal_hist, nc_placed_hist)
+            prem_pw_hist = per_week(prem_goal_hist, prem_placed_hist)
         
         # Calculate the lives, nc, and prem per week benchmarks.
         self.lives_per_week_benchmark = lives_goal / 52
         self.new_clients_per_week_benchmark = nc_goal / 52
         self.premium_per_week_benchmark = prem_goal / 52
+        
+        # Not calculating historical benchmarks right now because I'm not tracking historicial goal changes.
                 
-        # Set the results to the publically available vars
+        # Set the results to the publically available vars (current)
         self.lives_per_week = lives_pw
         self.new_clients_per_week = nc_pw
         self.premium_per_week = prem_pw
+        
+        # Set the results to the publically available vars (historical)
+        if xml_file_hist is not None: 
+            self.lives_per_week_hist = lives_pw_hist
+            self.new_clients_per_week_hist = nc_pw_hist
+            self.premium_per_week_hist = prem_pw_hist
+        
+        
         
     
     def _validate_xml_file(self, xml_file):
